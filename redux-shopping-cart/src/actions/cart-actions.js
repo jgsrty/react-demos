61d5dbe9d88a6e7099855e5e9f80bf2dacfa9f4c
jgsrty <573850993@qq.com>
2019-05-17 10:55:47 +0800

    add redux demo

---
+++ redux-shopping-cart/src/actions/cart-actions.js
@@ -0,0 +1,29 @@
+export const ADD_TO_CART = 'ADD_TO_CART'
+export const UPDATE_CART = 'UPDATE_CART'
+export const DELETE_FROM_CART = 'DELETE_FROM_CART'
+
+export function addToCart(product, quantity, unitCost) {
+  return {
+    type: ADD_TO_CART,
+    payload: { product, quantity, unitCost }
+  }
+}
+export function updateCart(product, quantity, unitCost) {
+  return {
+    type: UPDATE_CART,
+    payload: {
+      product,
+      quantity,
+      unitCost
+    }
+  }
+}
+
+export function deleteFromCart(product) {
+  return {
+    type: DELETE_FROM_CART,
+    payload: {
+      product
+    }
+  }
+}
--- redux-shopping-cart/src/index.js
+++ redux-shopping-cart/src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import store from './store.js';
+import { addToCart,updateCart,deleteFromCart }  from './actions/cart-actions';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+console.log("initial state: ", store.getState());
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+let unsubscribe = store.subscribe(() =>
+  console.log(store.getState())
+);
+
+store.dispatch(addToCart('Coffee 500gm', 1, 250));
+store.dispatch(addToCart('Flour 1kg', 2, 110));
+store.dispatch(addToCart('Juice 2L', 1, 250));
+
+// Update Cart
+store.dispatch(updateCart('Flour 1kg', 5, 110));
+// Delete from Cart
+store.dispatch(deleteFromCart('Coffee 500gm'));
+
+unsubscribe();
